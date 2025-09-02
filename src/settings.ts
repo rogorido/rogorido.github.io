c394eec4d56ac84ae296c4e7205fa6be1d399796
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2025-09-02 21:21:48 +0200

    Changing default theme.

--- src/settings.ts
+++ src/settings.ts
@@ -42,7 +42,8 @@ export const template = {
   website_url: "https://rogorido.github.io", // Astro needs to know your site’s deployed URL to generate a sitemap. It must start with http:// or https://
   menu_left: false,
   transitions: true,
-  lightTheme: "light", // Select one of the Daisy UI Themes or create your own
+  // lightTheme: "light", // Select one of the Daisy UI Themes or create your own
+  lightTheme: "lemonade", // Select one of the Daisy UI Themes or create your own
   darkTheme: "dark", // Select one of the Daisy UI Themes or create your own
   excerptLength: 200,
   postPerPage: 5,
