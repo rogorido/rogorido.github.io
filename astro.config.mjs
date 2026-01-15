14244bde4076de7829563984cc9399c87dca84ea
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2026-01-15 16:28:38 +0100

    no react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
+//import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 import { template } from "./src/settings";
@@ -10,7 +10,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind(), sitemap()],
+    integrations: [ tailwind(), sitemap()],
     site: template.website_url,
     base: template.base,
 });
